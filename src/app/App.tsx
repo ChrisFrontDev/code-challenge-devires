9cea8d5ea579b26233b7e2704cc74c97ae130612
Christian Santos <christian.pinho@outlook.com>
2020-11-07 03:49:18 -0300

    feat: show todolist

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,36 @@
+import ToDoList from 'components/ToDoList/ToDoList';
 import React from 'react';
+import GlobalStyle from '../styles/global';
+
+interface ToDo {
+  id: string;
+  title: string;
+  description: string;
+}
+
+const todoss: ToDo[] = [
+  {
+    id: '1',
+    title: 'Regar plantas',
+    description: 'Regar todas menos o cacto',
+  },
+  {
+    id: '2',
+    title: 'Passar no code challenge',
+    description:
+      'Caprichar no código e passar no code challenge mais facil do planeta',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Code Challenge!</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <h1>What&apos;s up, Olivia!</h1>
+        <ToDoList todos={todoss} />
+      </div>
+    </>
   );
 }
 
