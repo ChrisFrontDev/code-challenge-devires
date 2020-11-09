552796e3900024230dfd0d94f2bbc7d68b442af8
Christian Santos <christian.pinho@outlook.com>
2020-11-08 21:39:46 -0300

    refactor: cleaning App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,34 +1,15 @@
-import ToDoList from 'components/ToDoList/ToDoList';
 import React from 'react';
+
+import Home from '../pages/Home/Home';
+
 import GlobalStyle from '../styles/global';
-
-interface ToDo {
-  id: string;
-  title: string;
-  description: string;
-}
-
-const todoss: ToDo[] = [
-  {
-    id: '1',
-    title: 'Regar plantas',
-    description: 'Regar todas menos o cacto',
-  },
-  {
-    id: '2',
-    title: 'Passar no code challenge',
-    description:
-      'Caprichar no código e passar no code challenge mais facil do planeta',
-  },
-];
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <div className="App">
-        <h1>What&apos;s up, Olivia!</h1>
-        <ToDoList todos={todoss} />
+        <Home />
       </div>
     </>
   );
